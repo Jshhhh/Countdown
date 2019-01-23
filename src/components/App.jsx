803e4f85b6480c26012e1e503fa5dc64510e2a9a
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2019-01-22 20:55:04 -0800

    adds css file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import './App.scss'
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      
+      input: '',
+      events: [],
     };
   }
 
   render() {
     return (
-      <div>Hello</div>
+      <section className='main'>
+        <div className='header'>Countdown!</div>
+        <input></input>
+      </section>
     )
   }
 };
