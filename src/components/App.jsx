2a8388a8900505a75de28539138df61299d3a789
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2019-01-22 22:38:13 -0800

    Adds inputs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Calendar from 'react-calendar';
 import './App.scss'
 
 class App extends React.Component {
@@ -7,15 +8,40 @@ class App extends React.Component {
     this.state = {
       input: '',
       events: [],
+      showCalendar: false,
+      date: new Date(),
     };
+  }
+
+  onClickDateInput = () => {
+    this.setState({showCalendar: true});
+  }
+
+  onChangeEvent = e => {
+    this.setState({input: e.target.value})
+  }
+
+  onChangeDate = date => {
+    this.setState({
+      date,
+      showCalendar: false
+    })
   }
 
   render() {
     return (
       <section className='main'>
         <div className='header'>Countdown!</div>
-        <input placeholder='Add event'></input>
-      </section>
+        <input placeholder='Add event' value={this.state.input} onChange={this.onChangeEvent}>
+        </input>
+        <input placeholder='Add date' value={this.state.date} onClick={this.onClickDateInput}></input>
+        {
+          this.state.showCalendar ?
+          <Calendar onChange={this.onChangeDate} value={this.state.date}></Calendar> 
+          : null
+        }
+        <button onClick={this.onAddEvent}>Add Event</button>
+      </section> 
     )
   }
 };
